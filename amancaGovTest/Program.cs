114fb4035843a1c38f3c671a93c754444a46846c
Andrew Manca <anmanca@microsoft.com>
2020-10-28 11:32:58 -0400

    Updates

--- amancaGovTest/Program.cs
+++ amancaGovTest/Program.cs
@@ -29,18 +29,18 @@ namespace AlexDickersonAppService
                     webBuilder.UseStartup<Startup>();
                 }).ConfigureAppConfiguration(config =>
                 {
-                    //var configuration = config.Build();
-                    //int configCount1 = configuration.AsEnumerable().Count();
+                    var configuration = config.Build();
+                    int configCount1 = configuration.AsEnumerable().Count();
 
-                    ////Environment.SetEnvironmentVariable("AZURE_CLIENT_ID", "32b86793-9db5-40bc-84d6-f50a87859afc");
-                    ////Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", "Vro71px9_9VPYXHCI20j6~USP-6vGdyqV-");
-                    ////Environment.SetEnvironmentVariable("AZURE_TENANT_ID", "72f988bf-86f1-41af-91ab-2d7cd011db47");
-                    //var test = new DefaultAzureCredential();
+                    //Environment.SetEnvironmentVariable("AZURE_CLIENT_ID", "32b86793-9db5-40bc-84d6-f50a87859afc");
+                    //Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", "Vro71px9_9VPYXHCI20j6~USP-6vGdyqV-");
+                    //Environment.SetEnvironmentVariable("AZURE_TENANT_ID", "72f988bf-86f1-41af-91ab-2d7cd011db47");
+                    var test = new DefaultAzureCredential();
 
-                    //config.AddAzureAppConfiguration(options =>
-                    //{
-                    //    options.Connect(new Uri("https://amancaappconfigkv.azconfig.azure.us"), test).ConfigureKeyVault(kv => kv.SetCredential(test));
-                    //});
+                    config.AddAzureAppConfiguration(options =>
+                    {
+                        options.Connect(new Uri("https://amancaappconfigkv.azconfig.azure.us"), test).ConfigureKeyVault(kv => kv.SetCredential(test));
+                    });
 
                 }
                 );
